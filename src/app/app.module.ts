2bfa36711819a58d5c58c5d6921ad760a30de210
ParinayReddy <51450999+ParinayReddy@users.noreply.github.com>
2020-06-09 13:31:37 +0530

    LikeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeComponent } from './like.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/like.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component ({
+    selector: 'like-box',
+    templateUrl: 'like.component.html',
+})
+
+export class LikeComponent {
+    numberOfLikes : number=0;
+
+    likeButtonClick(){
+        this.numberOfLikes++; 
+    }
+
+}
